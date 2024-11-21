148d27e7d4dc191ce9a0911dd3fe9073c1ee9696
bgmnn <iamsaltgawd@gmail.com>
2024-11-21 16:40:06 +0200

    Initial commit

---
+++ src/main/java/com/bgmnn/laidatlamluat/LaidatlamluatApplication.java
@@ -0,0 +1,13 @@
+package com.bgmnn.laidatlamluat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaidatlamluatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaidatlamluatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bgmnn/laidatlamluat/LaidatlamluatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgmnn.laidatlamluat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaidatlamluatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
